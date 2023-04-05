159ead3c5452d0c2b4595e720d24022b59e37deb
Ri <ri@3sidedcube.com>
2023-04-05 13:51:28 +0100

    Handling Events

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,4 +1,5 @@
 import { Fragment } from "react";
+import { MouseEvent } from "react";
 
 let cities = [
   { name: "New York" },
@@ -7,24 +8,33 @@ let cities = [
   { name: "Tokyo" },
 ];
 
-cities = [];
+// cities = [];
 
 const getCities = () => {
   // can be a function if taking a param, better to be a var if not
   return cities.length === 0 && <p>No Cities Found Here.</p>;
 };
 
+const handleCityClicked = (
+  event: MouseEvent
+  // , city: string, index: number // Question --- want to pass these params with TS
+) => console.log(event);
+
 const ListGroup = () => {
   return (
-    // Question -- when should you use fragment, and when should div / section (other element be used)
     <>
+      {/* // Question -- when should you use fragment as above, and when should div / section (other element be used) */}
       <h1>Cities</h1>
       {getCities()}
       {cities.length === 0 && <p>No Cities Found.</p>}
 
       <ul className="list-group">
-        {cities?.map((city) => (
-          <li className="list-group-item" key={city.name}>
+        {cities?.map((city, index) => (
+          <li
+            className="list-group-item"
+            key={city.name}
+            onClick={handleCityClicked}
+          >
             {city.name}
           </li>
         ))}
