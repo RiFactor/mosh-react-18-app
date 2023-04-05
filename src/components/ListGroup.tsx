097ec5b07836dcb8badf5c71aced218a2353d2d3
Ri <ri@3sidedcube.com>
2023-04-05 16:22:46 +0100

    What: pass params w/ TS

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -3,30 +3,24 @@ import { MouseEvent } from "react";
 import classNames from "classnames";
 
 interface IItemsProps {
-  // items: ItemsProps[];
-  items: ItemProps[];
+  items: IItemProps[];
   heading: string;
 }
 
-interface ItemProps {
+interface IItemProps {
   name: string;
 }
 
 const ListGroup = ({ items, heading }: IItemsProps) => {
-  // cities = [];
+  const [selectedCity, setSelectedCity] = useState(-1); // NTS: copies of components will have their own state, if we import listgroup twice into the app, the states will be independent
 
-  const [selectedCity, setSelectedCity] = useState(-1); // copies of components will have their own state, if we import listgroup twice into the app, the states will be independent
+  const getCities = items.length === 0 && <p>No {heading} Found Here.</p>; // NTS: can be a function if taking a param, better to be a var if not
 
-  const getCities = () => {
-    // can be a function if taking a param, better to be a var if not
-    return items.length === 0 && <p>No {heading} Found Here.</p>;
-  };
-
-  // const handleCityClicked = (
-  //   event: MouseEvent
-  //   // city: string,
-  //   // index: number // QUESTION --- want to pass these params with TS
-  // ) => console.log(event);
+  const handleCityClicked = (
+    event: MouseEvent,
+    index: number,
+    name: string // NTS: param order matters!
+  ) => console.log("test", index + 1, name, event);
 
   const handleSelectedCity = (index: number) => {
     selectedCity === index ? setSelectedCity(-1) : setSelectedCity(index);
@@ -35,17 +29,19 @@ const ListGroup = ({ items, heading }: IItemsProps) => {
   return (
     <>
       <h1>{heading}</h1>
-      {getCities()}
+      {getCities}
       {items.length === 0 && <p>No {heading} Found.</p>}
 
       <ul className="list-group">
         {items?.map((item, index) => (
           <li
-            className={
-              classNames("list-group-item", selectedCity === index && "active") // used classNames import to concatenate :)
-            }
+            className={classNames(
+              "list-group-item",
+              selectedCity === index && "active"
+            )}
             key={item.name}
-            onClick={() => {
+            onClick={(event) => {
+              handleCityClicked(event, index, item.name);
               handleSelectedCity(index);
             }}
           >
