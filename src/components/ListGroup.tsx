5dc42c19d5698bcb65481d6caa1ef1c381ead74a
Ri <ri@3sidedcube.com>
2023-04-05 12:15:17 +0100

    What: Conditional Rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,23 +1,30 @@
 import { Fragment } from "react";
 
-const cities = [
+let cities = [
   { name: "New York" },
   { name: "Paris" },
   { name: "London" },
   { name: "Tokyo" },
 ];
 
+// cities = [];
+
 const ListGroup = () => {
   return (
     // Question -- when should you use fragment, and when should div / section (other element be used)
     <>
+      <h1>Cities</h1>
+      {cities.length === 0 && <p>No Cities Found.</p>}
+
       <ul className="list-group">
-        {cities.map((city) => (
+        {cities?.map((city) => (
           <li className="list-group-item" key={city.name}>
             {city.name}
           </li>
         ))}
       </ul>
+      <br />
+      <p> test</p>
     </>
   );
 };
