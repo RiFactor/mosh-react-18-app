eb61111223fc6d2ae8fae3f27efecb2bca73cf97
Ri <ri@3sidedcube.com>
2023-04-05 12:16:22 +0100

    What: Conditional Rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -7,13 +7,19 @@ let cities = [
   { name: "Tokyo" },
 ];
 
-// cities = [];
+cities = [];
+
+const getCities = () => {
+  // can be a function if taking a param, better to be a var if not
+  return cities.length === 0 && <p>No Cities Found Here.</p>;
+};
 
 const ListGroup = () => {
   return (
     // Question -- when should you use fragment, and when should div / section (other element be used)
     <>
       <h1>Cities</h1>
+      {getCities()}
       {cities.length === 0 && <p>No Cities Found.</p>}
 
       <ul className="list-group">
