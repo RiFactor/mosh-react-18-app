ef2482609f7d910e2a3636f2e992b7d83b3b1d1f
Ri <ri@3sidedcube.com>
2023-04-05 14:17:37 +0100

    What: Managing State

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,29 +1,36 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import { MouseEvent } from "react";
 
-let cities = [
-  { name: "New York" },
-  { name: "Paris" },
-  { name: "London" },
-  { name: "Tokyo" },
-];
-
-// cities = [];
-
-const getCities = () => {
-  // can be a function if taking a param, better to be a var if not
-  return cities.length === 0 && <p>No Cities Found Here.</p>;
-};
-
-const handleCityClicked = (
-  event: MouseEvent
-  // , city: string, index: number // Question --- want to pass these params with TS
-) => console.log(event);
-
 const ListGroup = () => {
+  let cities = [
+    { name: "New York" },
+    { name: "Paris" },
+    { name: "London" },
+    { name: "Tokyo" },
+  ];
+
+  // cities = [];
+
+  const [selectedCity, setSelectedCity] = useState(-1);
+
+  const getCities = () => {
+    // can be a function if taking a param, better to be a var if not
+    return cities.length === 0 && <p>No Cities Found Here.</p>;
+  };
+
+  // const handleCityClicked = (
+  //   event: MouseEvent
+  //   // city: string,
+  //   // index: number // QUESTION --- want to pass these params with TS
+  // ) => console.log(event);
+
+  const handleSelectedCity = (index: number) => {
+    selectedCity === index ? setSelectedCity(-1) : setSelectedCity(index);
+  };
+
   return (
     <>
-      {/* // Question -- when should you use fragment as above, and when should div / section (other element be used) */}
+      {/* // QUESTION -- when should you use fragment as above, and when should div / section (other element be used) */}
       <h1>Cities</h1>
       {getCities()}
       {cities.length === 0 && <p>No Cities Found.</p>}
@@ -31,9 +38,16 @@ const ListGroup = () => {
       <ul className="list-group">
         {cities?.map((city, index) => (
           <li
-            className="list-group-item"
+            className={
+              selectedCity === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
             key={city.name}
-            onClick={handleCityClicked}
+            onClick={() => {
+              // setSelectedCity(index);
+              handleSelectedCity(index);
+            }}
           >
             {city.name}
           </li>
