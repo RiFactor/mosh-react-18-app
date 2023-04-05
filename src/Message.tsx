26ca38f17d48124bf60441aecfe02ebb2ea2fc96
Ri <ri@3sidedcube.com>
2023-04-05 11:28:00 +0100

    What: -  install bootstrap, - create a listgroup component, - fragments - cleared default styling - conditionally render welcome message

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,11 +1,18 @@
 const Message = () => {
-  const name = "";
+  const name = "Ri";
 
-  if (name) {
-    return <h1>Hello, {name}</h1>;
-  } else {
-    return <h1>Hello, World!</h1>;
-  }
+  //   if (name) {
+  //     return <h1>Hello, {name}</h1>;
+  //   } else {
+  //     return <h1>Hello, World!</h1>;
+  //   }
+
+  return (
+    <div>
+      {name && <h1>Hello, {name}</h1>}
+      <h3>How are you doing?</h3>
+    </div>
+  );
 };
 
 export default Message;
