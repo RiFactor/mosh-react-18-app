fc9e9e8a96220d17e11cc80e8356824064b848c0
Ri <ri@3sidedcube.com>
2023-04-12 16:03:11 +0100

    Exercise: Building an ExpandableText Component

--- src/components/ExpandableText.tsx
+++ src/components/ExpandableText.tsx
@@ -5,8 +5,22 @@ const ExpandableText = () => {
   const [showText, setShowText] = useState(false);
   const [text, setText] = useState("Expandable Text Over a character limit");
 
+  const [expandableText, setExpandableText] = useState({
+    // QUESTION -- is it preferred to store these related items in one state?
+    charLimit: 10,
+    showText: false,
+    text: "I bought dog flea treatment and later relaxed with a cup of tea then realised I'd left my shoehorn in the car.",
+  });
+
   const handleShowText = () => {
     setShowText(!showText);
+  };
+
+  const handleExpandableText = () => {
+    setExpandableText({
+      ...expandableText,
+      showText: !expandableText.showText,
+    });
   };
 
   return (
@@ -15,6 +29,16 @@ const ExpandableText = () => {
       <button style={{ margin: "0.125rem" }} onClick={handleShowText}>
         {showText ? "Less" : "More"}
       </button>
+
+      {/* Alternate version in one state */}
+      <p>
+        {expandableText.showText
+          ? expandableText.text
+          : expandableText.text.substring(0, expandableText.charLimit)}
+        <button style={{ margin: "0.125rem" }} onClick={handleExpandableText}>
+          {expandableText.showText ? "Less" : "More"}
+        </button>
+      </p>
     </div>
   );
 };
