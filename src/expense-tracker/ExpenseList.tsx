2f1d96c85c18d67054bf2e08182a18ba8dce7eeb
Ri <ri@3sidedcube.com>
2023-04-17 19:10:07 +0100

    Solution: Expense Tracker - separate list component

--- src/expense-tracker/ExpenseList.tsx
+++ src/expense-tracker/ExpenseList.tsx
@@ -1,209 +1,63 @@
-import { FieldValues, useForm } from "react-hook-form";
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { ChangeEvent, useState } from "react";
+type IExpense = {
+  // DIFF - type, not interface
+  id: number;
+  description: string;
+  amount: number;
+  category: string;
+  // "Groceries" | "Utlities" | "Toiletries" | "Entertainment"; // DIFF - put options, not string; // ToDo: add 'id' and value
+};
 
-const minimumLength = 3;
+interface IProps {
+  // QUESTION -- why is type = {}; but no '=' for IProps, not Props = {}, just Props {}
+  expenses: IExpense[];
+  onDelete: (id: number) => void;
+}
 
-const schema = z.object({
-  description: z
-    .string()
-    .nonempty({ message: "Please provide a description." })
-    .min(minimumLength, { message: "Please enter at least 2 characters." }),
-  amount: z
-    .number({ invalid_type_error: "Number is required" })
-    .min(0.01, { message: "Amount must be at least £0.01." })
-    .nonnegative({ message: "Amount cannot be negative." }),
-  category: z.enum(["groceries", "utilities", "toiletries"]),
-});
-
-const options = [
-  { value: "groceries", label: "Groceries" },
-  { value: "utilities", label: "Utilities" },
-  { value: "toiletries", label: "Toiletries" },
-];
-
-type TExpense = z.infer<typeof schema>;
-
-const ExpenseTracker = () => {
-  const [selectedExpenseCategory, setSelectedExpenseCategory] = useState("");
-
-  const [expenses, setExpenses] = useState([
-    {
-      id: 1,
-      description: "test",
-      amount: 1,
-      category: "groceries",
-    },
-    {
-      id: 2,
-      description: "test2",
-      amount: 2,
-      category: "utilities",
-    },
-  ]);
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors, isValid },
-  } = useForm<TExpense>({ resolver: zodResolver(schema) });
-
-  const onSubmit = (data: FieldValues) => {
-    setExpenses([
-      ...expenses,
-      {
-        id: expenses.length + 1,
-        description: "item",
-        amount: 1.5,
-        category: "utilities",
-      },
-    ]);
-    console.log(data, "submit data");
-    console.log(expenses, "final expenses");
-  };
-
-  const handleDelete = (id: number) => {
-    console.log(expenses);
-
-    setExpenses(expenses.filter((expense) => expense.id !== id));
-    console.log(id);
-  };
-
-  const filteredList = () => {
-    const filteredExpenses = expenses.filter((expense) =>
-      selectedExpenseCategory === ""
-        ? expense
-        : expense.category === selectedExpenseCategory
-    );
-
-    let amount = 0;
-
-    filteredExpenses.map((expense) => {
-      amount += expense.amount;
-    });
-
-    console.log(filteredExpenses);
-    return amount;
-  };
-
+const ExpenseList = ({ expenses, onDelete }: IProps) => {
   return (
-    <>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <h3>Expense Tracker</h3>
-        <div className="mb-3">
-          <label htmlFor="description" className="form-label">
-            Description
-          </label>
-          <input
-            {...register("description")}
-            id="description"
-            type="text"
-            className="form-control"
-          />
-          {errors.description && (
-            <p className="text-danger">{errors.description.message}</p>
-          )}
-        </div>
-        <div className="mb-3">
-          <label htmlFor="amount" className="form-label">
-            Amount
-          </label>
-          <input
-            {...register("amount", { valueAsNumber: true })}
-            id="amount"
-            type="number"
-            className="form-control"
-          />
-          {errors.amount && (
-            <p className="text-danger">{errors.amount.message}</p>
-          )}
-        </div>
-        <div className="mb-3">
-          <label htmlFor="category" className="form-label">
-            Category
-          </label>
-          <select
-            {...register("category")}
-            id="category"
-            className="form-control"
-          >
-            {options.map((option) => {
-              return (
-                <option key={option.value} value={option.value}>
-                  {option.label}
-                </option>
-              );
-            })}
-          </select>
-          {errors.category && (
-            <p className="text-danger">{errors.category.message}</p>
-          )}
-        </div>
-        <button className="btn btn-primary">Submit</button>
-      </form>
-      <div>
-        <h1>Expenses</h1>
+    <table className="table table-bordered">
+      <thead>
+        <tr>
+          <th>Description</th>
+          <th>Amount (£)</th>
+          <th>Category</th>
+          <th></th>
+        </tr>
+      </thead>
 
-        <select
-          className="dropdown-header"
-          value={selectedExpenseCategory}
-          onChange={(event) => {
-            setSelectedExpenseCategory(event.target.value);
-          }}
-        >
-          <option value="">Everything</option>
-          {options.map((option) => {
-            return (
-              <option key={option.value} value={option.value}>
-                {option.label}
-              </option>
-            );
-          })}
-        </select>
-
-        <table className="table">
-          <thead>
-            <tr>
-              <th scope="col">Description</th>
-              <th scope="col">Amount (£)</th>
-              <th scope="col">Category</th>
-              <th></th>
+      <tbody>
+        {expenses.map((expense) => {
+          return (
+            <tr key={expense.id}>
+              <td>{expense.description}</td>
+              <td>{expense.amount.toFixed(2)}</td>
+              <td>{expense.category}</td>
+              <td>
+                <button
+                  className="btn btn-outline-danger"
+                  onClick={() => onDelete(expense.id)}
+                >
+                  Delete
+                </button>
+              </td>
             </tr>
-          </thead>
-          <tbody>
+          );
+        })}
+      </tbody>
+      <tfoot>
+        <tr>
+          <td>Total</td>
+          <td>
             {expenses
-              .filter((expense) =>
-                selectedExpenseCategory === ""
-                  ? expense
-                  : expense.category === selectedExpenseCategory
-              )
-              .map((expense: any, index: number) => {
-                return (
-                  <tr key={index}>
-                    <th scope="row">{expense.description}</th>
-                    <td>{expense.amount}</td>
-                    <td>{expense.category}</td>
-                    <td>
-                      <button
-                        className="btn btn-danger"
-                        onClick={() => handleDelete(expense.id)}
-                      >
-                        Delete
-                      </button>
-                    </td>
-                  </tr>
-                );
-              })}
-            <tr>
-              <th>Total</th>
-              <th>£ {filteredList()}</th>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </>
+              .reduce((acc, expense) => acc + expense.amount, 0)
+              .toFixed(2)}
+          </td>
+          <td></td>
+          <td></td>
+        </tr>
+      </tfoot>
+    </table>
   );
 };
 
-export default ExpenseTracker;
+export default ExpenseList;
--- src/expense-tracker/ExpenseView.tsx
+++ src/expense-tracker/ExpenseView.tsx
@@ -1,9 +1,33 @@
+import { useState } from "react";
+import ExpenseFilter from "./ExpenseFilter";
 import ExpenseList from "./ExpenseList";
+import ExpenseTracker from "./ExpenseTracker";
 
 const ExpenseView = () => {
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "games", amount: 5, category: "Entertainment" },
+    { id: 2, description: "books", amount: 2, category: "Entertainment" },
+    { id: 3, description: "shampoo", amount: 4, category: "Toiletries" },
+    { id: 4, description: "lightbulb", amount: 1, category: "Utilities" },
+  ]);
+
+  const handleDelete = (id: number) => {
+    setExpenses(expenses.filter((expense) => expense.id !== id));
+  };
+
+  if (expenses.length === 0) return null;
+
   return (
     <div>
-      <ExpenseList />
+      <div className="m-3">
+        <ExpenseFilter />
+      </div>
+      <div className="m-3">
+        <ExpenseList expenses={expenses} onDelete={(id) => handleDelete(id)} />
+      </div>
+      <div className="m-3">
+        <ExpenseTracker />
+      </div>
     </div>
   );
 };
