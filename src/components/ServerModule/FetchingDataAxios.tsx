9c2defe8139e54db3d5121d9aad2d53aee894b89
Ri <ri@3sidedcube.com>
2023-04-20 15:55:22 +0100

    Connecting to the Backend - Handling Errors

--- src/components/ServerModule/FetchingDataAxios.tsx
+++ src/components/ServerModule/FetchingDataAxios.tsx
@@ -10,15 +10,18 @@ type TUsers = {
 
 const FetchingDataAxios = () => {
   const [users, setUsers] = useState<TUsers[]>([]); // NTS: must remember array here
+  const [errors, setErrors] = useState("");
 
   useEffect(() => {
     axios
-      .get<TUsers[]>("https://jsonplaceholder.typicode.com/users") // NTS: must remember array here
-      .then((res) => setUsers(res.data));
+      .get<TUsers[]>("https://jsonplaceholder.typicode.com/xusers") //incorrect end point to demo error; NTS: must remember array here
+      .then((res) => setUsers(res.data))
+      .catch((err) => setErrors(err.message));
   }, []);
 
   return (
     <div>
+      {errors && <p className="text-danger">{errors}</p>}
       <h3>Fetching Data Axios</h3>
       <ul>
         {users.map((user) => (
